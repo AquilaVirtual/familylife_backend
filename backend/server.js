b7fd2de9452fa86f39ec174574d2bdde4d3bedd8
David Loveday <davemech@yahoo.com>
2019-01-10 23:37:53 -0800

    server setup

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+
+//Instantiate Server
+const server = express();
+
+//Bringin Mongoose Database
+const mongoose = require("mongoose");
+
+ const db = "mongodb://aquila:familylife123@ds048319.mlab.com:48319/family-life";
+ 
+//Connect Database
+mongoose
+  .connect(db)
+  .then(() => console.log("\n=== connected to mongo ===\n"))
+  .catch(err => console.log("database is not connected"));
+
+//Security
+server.use(helmet());
+
+//Insanity check
+server.get('/', (request, response) => {
+response.send("API: running")
+})
+
+//Permissions
+server.use(cors());
+
+//Enable to parse Json object
+server.use(express.json());
+server.use(bodyparser.json()); //express.jason;
+
+server.use(require("body-parser").text());
+
+//Status server
+const port = process.env.PORT || 3002;
+server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
