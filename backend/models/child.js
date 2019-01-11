8516a5ab92c6674a1b220b26f683d6c259b4d796
David Loveday <davemech@yahoo.com>
2019-01-10 23:52:15 -0800

    add parent and child models

---
+++ backend/models/child.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const childSchema = new mongoose.Schema({ 
+
+  // Display name
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  // login name
+  username: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Parent",
+    required: true, 
+  },
+
+});
+
+let childModel = mongoose.model("Child", childSchema);
+
+module.exports = childModel;
---
+++ backend/models/parent.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const parentSchema = new mongoose.Schema({ 
+
+  // Display name
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  // login name
+  username: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+  },
+
+});
+
+let parentModel = mongoose.model("Parent", parentSchema);
+
+module.exports = parentModel;
