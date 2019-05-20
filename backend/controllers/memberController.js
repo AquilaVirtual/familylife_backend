cb78bd821890fd768904b3f76ed34a4712c43bbd
David Loveday <davemech@yahoo.com>
2019-05-19 17:50:00 -0700

    fix bug

--- backend/controllers/memberController.js
+++ backend/controllers/memberController.js
@@ -85,12 +85,15 @@ const createMember = (request, response) => {
                  Famliy Life
                  ` 
                 let transporter = nodemailer.createTransport({
-                    service: 'gmail.com',
+                    service: 'smpt.gmail.com',
                     port: 587,
                     secure: false,
                     auth: {
                       user: process.env.NODEMAILER_USER,
                       pass: process.env.NODEMAILER_PASS 
+                    },
+                    tls: {
+                      rejectUnauthorized: true
                     }
                   });                  
                 let mailOptions = {
